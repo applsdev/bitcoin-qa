b2748b19b6fead05d369cc2109db313bff047431
David FRANCOIS <david.francois@webflows.fr>
2011-07-04 21:55:38 +0200

    App skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitcoinQa::Application.config.secret_token = '26c296939310b8a69086d721c80db7867e7ce7ef2f60268573322f9d918e8a346e2d8ed167cbb04765e299e65559f9bf6a284bd2c8dbfbc92a16319e4a4951e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BitcoinQa::Application.config.session_store :cookie_store, :key => '_bitcoin-qa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BitcoinQa::Application.config.session_store :active_record_store
